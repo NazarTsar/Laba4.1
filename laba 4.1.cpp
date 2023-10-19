a49736e9bb059d222243c49bace3ed7708bac5b8
NazarTsar <146090679+NazarTsar@users.noreply.github.com>
2023-10-19 21:48:36 +0300

    Update laba 4.1.cpp

--- laba 4.1.cpp
+++ laba 4.1.cpp
@@ -1,46 +1,52 @@
-// Laba_04_1.cpp 
-
-// ??? ????? 
-
-// ??????????? ?????? ? 4.1 
-
-// ????? 
-
-// ??????? 24 
 #include <iostream>
 #include <cmath>
-using namespace std;
+
 int main()
 {
-	int N, i;
-	double S;
-	cout << "N = "; cin >> N;
-	S = 0;
-	i = 1;
-	while (i <= N)
-	{
-		S += (sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2))) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
-		i++;
-	}
-	cout << S << endl;
-	S = 0;
-	i = 1;
-	do {
-		S += (sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2))) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
-		i++;
-	} while (i <= N);
-	cout << S << endl;
-	S = 0;
-	for (i = 1; i <= N; i++)
-	{
-		S += (sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2))) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
-	}
-	cout << S << endl;
-	S = 0;
-	for (i = 1; i >= 1; i--)
-	{
-		S += (sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2))) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
-	}
-	cout << S << endl;
-	return 0;
+    int N, i;
+    double S;
+
+    std::cout << "N = ";
+    std::cin >> N;
+
+    S = 0;
+    i = 1;
+
+    // Перший цикл: while
+    while (i <= N)
+    {
+        S += std::sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2)) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
+        i++;
+    }
+    std::cout << "Result for 'while': " << S << std::endl;
+
+    S = 0;
+    i = 1;
+
+    // Другий цикл: do-while
+    do
+    {
+        S += std::sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2)) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
+        i++;
+    } while (i <= N);
+    std::cout << "Result for 'do-while': " << S << std::endl;
+
+    S = 0;
+
+    // Третій цикл: for
+    for (i = 1; i <= N; i++)
+    {
+        S += std::sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2)) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
+    }
+    std::cout << "Result for 'for': " << S << std::endl;
+
+    // Четвертий цикл: for (змінено умову для завершення циклу)
+    S = 0;
+    for (i = N; i >= 1; i--)
+    {
+        S += std::sqrt(1.0 + std::pow(std::cos(std::sin(i)), 2)) / (1.0 + std::pow(std::sin(std::cos(i)), 2));
+    }
+    std::cout << "Result for 'reversed for': " << S << std::endl;
+
+    return 0;
 }
